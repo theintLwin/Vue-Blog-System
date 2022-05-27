79798f00475aa08f2af12e45f75de7e4e53b06be
theintLwin <theinttheintlwin38@gmail.com>
2022-05-27 14:36:59 +0630

    Fetch our single post document

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,4 +1,5 @@
 import { ref } from "vue";
+import {db} from "../firebase/config"
 
 let getPost = (id) => {
     let post=ref(null);
@@ -7,15 +8,9 @@ let getPost = (id) => {
     let load = async ()=>{
         
         try{
-            // await new Promise((resolve,reject)=>{
-            //     setTimeout(resolve, 2000);
-            // })
-            let response=await fetch("http://localhost:3000/posts/" + id);
-            if(response.status===404){
-                throw new Error("not fnd URL");
-            }
-            let datas= await response.json();
-            post.value = datas;
+            let doc = await db.collection("posts").doc(id).get();
+            post.value = {id:doc.id, ...doc.data()};
+           
         }
         catch(err){
             
