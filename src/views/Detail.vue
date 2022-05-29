7184e0a49c7b041832ed65613bae271817866326
theintLwin <theinttheintlwin38@gmail.com>
2022-05-29 10:03:15 +0630

    delete post from firebase

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,6 +3,7 @@
   <div v-if="post" class="post">
       <h2>{{post.title}}</h2>
       <p>{{post.body}}</p>
+      <button class="delete" @click="deletePost">delete</button>
   </div>
   <div v-else>
       <Spinner></Spinner>                   
@@ -14,17 +15,28 @@
 <script>
 import Spinner from '../components/Spinner'
 import getPost from "../composables/getPost"
-import {useRoute} from "vue-router"
+import {useRoute, useRouter} from "vue-router"
+
+import {db} from "../firebase/config"
+
 export default {
   components: { Spinner },
      props:['id'],
     
     setup(props){
       let route = useRoute();
+      let router = useRouter();
       let id = route.params.id;
         let {post,error,load} = getPost(id);
         load();
-        return{post, error};
+
+        let deletePost = async()=>{
+          let id = props.id;
+          await db.collection("posts").doc(id).delete();
+          router.push("/");
+        }
+        
+        return{post, error,deletePost};
     }
 }
 </script>
@@ -63,5 +75,8 @@ export default {
         padding: 8px;
         border-radius: 20px;
         font-size: 14px;
+      }
+      button.delete{
+        margin: 30px auto;
       }
 </style>
